6965bc040ccdd581d43c7808f4b4796912edcb05
alstn38 <alstn38@naver.com>
2023-07-19 15:33:44 +0900

    initial setting

---
+++ SejongCommunity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SejongCommunity
+//
+//  Created by 강민수 on 2023/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
