6d2148e9c38127b0e85d5bf2b62d5db440947be5
jeoungsung12 <jeoungsung12@naver.com>
2023-07-24 16:31:24 +0900

    [Fix] mainVC 배경색 변경

--- SejongCommunity/VC/MainViewController.swift
+++ SejongCommunity/VC/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
     }
     
