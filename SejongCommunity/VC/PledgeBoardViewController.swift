cc1547cfe920df3cc87ced31070623b043c10e3b
alstn38 <alstn38@naver.com>
2023-08-08 20:57:29 +0900

    [Fix] PledgeBoardViewController 편집모드 시 Cell 클릭 가능 변경

--- SejongCommunity/VC/PledgeBoardViewController.swift
+++ SejongCommunity/VC/PledgeBoardViewController.swift
@@ -89,7 +89,85 @@ class PledgeBoardViewController: UIViewController {
         
         setupUI()
     }
+
+}
+
+extension PledgeBoardViewController: UITableViewDelegate, UITableViewDataSource{
     
+    // 섹션의 갯수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //각 섹션 마다 cell row 숫자의 갯수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 15
+    }
+    
+    // 각 센션 마다 사용할 cell의 종류
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PledgeTableViewCell", for: indexPath) as! PledgeTableViewCell
+        
+        // 드래그 삭제 기능을 허용할 때만 셀의 드래그 삭제 스타일 설정
+//        if 학생회 {
+//            cell.showsReorderControl = true
+//            cell.selectionStyle = .default
+//        } else {
+//            cell.showsReorderControl = false
+//            cell.selectionStyle = .none
+//        }
+        
+        return cell
+    }
+    
+    //Cell의 높이를 지정한다.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 사용자가 셀을 선택했을 때 호출되는 메서드
+        // indexPath를 사용하여 선택된 셀의 정보를 가져올 수 있음
+        
+        let selectedRow = indexPath.row
+        print("Selected row: \(selectedRow)")
+        
+        if let cell = tableView.cellForRow(at: indexPath) as? PledgeTableViewCell {
+            // isSelected 값을 토글
+            cell.isCompletion.toggle()
+            // 선택된 셀의 레이아웃을 업데이트
+            cell.setup()
+        }
+        
+        // 선택된 셀에 대한 추가 작업을 수행
+        // 예를 들어, 다음 뷰 컨트롤러로 이동하거나 상세 정보를 표시하는 등의 작업
+    }
+    
+    //편집모드일때만 cell클릭할수있도록 변경
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if isEditingMode {
+            return indexPath
+        } else {
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // 선택한 행을 삭제하고 테이블 뷰에서도 삭제
+            // 여기에 실제 삭제 작업을 구현하세요
+            tableView.beginUpdates()
+            // 삭제 작업 (예: 데이터 소스에서 항목 제거)
+            // data.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+    }
+}
+
+private extension PledgeBoardViewController {
     func setupNavigationBar() {
         let editButtonTitle = isEditingMode ? "저장" : "편집"
         navigationItem.rightBarButtonItem = UIBarButtonItem(
@@ -141,6 +219,7 @@ class PledgeBoardViewController: UIViewController {
         
         // 네비게이션 바 우측 버튼 텍스트 변경
         setupNavigationBar()
+        pledgeTableView.reloadData()
     }
     
     @objc func pledgeMenuButtonTapped(_ sender: UIButton) {
@@ -162,54 +241,4 @@ class PledgeBoardViewController: UIViewController {
         
         pledgeTableView.reloadData()
     }
-
 }
-
-extension PledgeBoardViewController: UITableViewDelegate, UITableViewDataSource{
-    
-    // 섹션의 갯수
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    //각 섹션 마다 cell row 숫자의 갯수
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 15
-    }
-    
-    // 각 센션 마다 사용할 cell의 종류
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PledgeTableViewCell", for: indexPath) as! PledgeTableViewCell
-        
-        // 드래그 삭제 기능을 허용할 때만 셀의 드래그 삭제 스타일 설정
-//        if 학생회 {
-//            cell.showsReorderControl = true
-//            cell.selectionStyle = .default
-//        } else {
-//            cell.showsReorderControl = false
-//            cell.selectionStyle = .none
-//        }
-        
-        return cell
-    }
-    
-    //Cell의 높이를 지정한다.
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // 선택한 행을 삭제하고 테이블 뷰에서도 삭제
-            // 여기에 실제 삭제 작업을 구현하세요
-            tableView.beginUpdates()
-            // 삭제 작업 (예: 데이터 소스에서 항목 제거)
-            // data.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            tableView.endUpdates()
-        }
-    }
-}
-
