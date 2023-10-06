520d4dc79919370706505cf4e1a2d76d47779982
Minsoo Kang <alstn38@naver.com>
2023-10-06 23:25:20 +0900

    [Add] APIConstants 파일 추가

---
+++ SejongCommunity/Service/APIContants.swift
@@ -0,0 +1,20 @@
+//
+//  APIContants.swift
+//  SejongCommunity
+//
+//  Created by 강민수 on 2023/10/02.
+//
+
+import Foundation
+
+struct APIConstants {
+    // Base URL
+    static let baseURL = "http://15.164.161.53:8082"
+    
+    // Feature URL
+    
+    //학생회의 정보를 조회하는 api
+    static let councilInfoURL = baseURL + "/api/v1/council/info"
+    
+    
+}
