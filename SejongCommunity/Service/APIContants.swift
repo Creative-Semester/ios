c050f6b10c1fd0b4a6aee5ca67dfc9355c7642bf
Minsoo Kang <alstn38@naver.com>
2023-10-09 01:19:38 +0900

    [Add] APIConstants 학생회 공약 이행률, 부서목록, 공약 이행 URL 추가

--- SejongCommunity/Service/APIContants.swift
+++ SejongCommunity/Service/APIContants.swift
@@ -16,5 +16,12 @@ struct APIConstants {
     //학생회의 정보를 조회하는 api
     static let councilInfoURL = baseURL + "/api/v1/council/info"
     
+    //부서별 공약의 이행률을 조회하는 api
+    static let PromisesPercentageURL = baseURL + "/api/v1/department/promises/percentage"
     
+    //학생회에 존재하는 부서목록 조회 api
+    static let departmentURL = baseURL + "/api/v1/department"
+    
+    //학생회 공약 이행 체크 api
+    static let promiseCheckURL = baseURL + "/api/v1/promise"
 }
