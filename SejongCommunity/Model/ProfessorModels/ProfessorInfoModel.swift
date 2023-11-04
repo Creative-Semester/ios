923e93b6c82d4f43ab17f2d450ad72d890d87de6
Minsoo Kang <alstn38@naver.com>
2023-11-04 20:13:03 +0900

    [Feat] 교수게시판 교수님 정보 통신 구현

---
+++ SejongCommunity/Model/ProfessorModels/ProfessorInfoModel.swift
@@ -0,0 +1,25 @@
+//
+//  ProfessorInfoModel.swift
+//  SejongCommunity
+//
+//  Created by 강민수 on 11/4/23.
+//
+
+import Foundation
+
+struct ProfessorInfoResponse: Codable {
+    let time: String
+    let status: Int
+    let code, message: String
+    let result: ProfessorInfoResult
+}
+
+struct ProfessorInfoResult: Codable {
+    let totalPage, currentPage: Int
+    let list: [ProfessorInfo]
+}
+
+struct ProfessorInfo: Codable {
+    let professorId: Int
+    let name, intro, image: String
+}
