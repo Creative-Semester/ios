74714bc998062a7b563367c2f9b5afe8139da550
Minsoo Kang <alstn38@naver.com>
2023-11-27 23:32:09 +0900

    [Feat] 채팅방 채팅내용 통신 구현

--- SejongCommunity/Model/ChattingModels/ChatRoomListModel.swift
+++ SejongCommunity/Model/ChattingModels/ChatRoomListModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct ChatRoomListModelResponse: Codable {
+    let time: String
+    let status: Int
+    let code: String
+    let message: String
+    let result: ChatGetRoomListInfo
+}
+
+struct ChatGetRoomListInfo: Codable {
+    let chatRoomDetailInfoResponseList: [ChatRoomDetailInfoResponseList]
+    let currentPage, totalPages: Int
+}
+
+struct ChatRoomDetailInfoResponseList: Codable {
+    let boardName: String
+    let noteInfos: ChatNoteInfo
+    let receiverStudentNum: String
+    let roomId: Int
+    let senderStudentNum: String
+}
